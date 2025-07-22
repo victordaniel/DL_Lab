4a674322a5790a7f9bfaa860066c5814ac2f219d
Victor Daniel <victor.nitk@gmail.com>
2025-07-22 11:23:58 +0530

    initial

---
+++ venv/tempCodeRunnerFile.py
@@ -0,0 +1,28 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+#Step 2: Load & Preprocess MNIST Data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.astype('float32') / 255.0
+X_test = X_test.astype('float32') / 255.0
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+#Step 3: Build MLP Model
+model = Sequential()
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+#Step 4: Compile & Train
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+history = model.fit(X_train, y_train, epochs=5, batch_size=128, verbose=2,
+validation_split=0.1)
+
+#Step 5: Evaluate
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(f"Test Accuracy: {accuracy:.4f}")
